f57c58e006bf5fae726b76d174ddaadea1083e6f
xiidot1303 <xii1303@inbox.ru>
2024-11-05 15:50:34 +0500

    add sth in last commit

--- bot/bot/subscription.py
+++ bot/bot/subscription.py
@@ -5,7 +5,7 @@ from app.services.channel_access_service import *
 
 async def cancel_subscription(update: Update, context: CustomContext):
     # remove inline keyboards
-    # await bot_edit_message_reply_markup(update, context)
+    await bot_edit_message_reply_markup(update, context)
     
     *args, subscription_id = str(update.callback_query.data).split("--")
     subscription: Subscription = await get_subscription_by_id(subscription_id)
