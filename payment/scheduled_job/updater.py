aeb9aceece652f3b25d289a4338c297bd25c1029
xiidot1303 <xii1303@inbox.ru>
2024-12-02 12:03:51 +0500

    update_access_token on in production

--- payment/scheduled_job/updater.py
+++ payment/scheduled_job/updater.py
@@ -2,11 +2,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import register_events, DjangoJobStore
 from payment.scheduled_job import atmos_job
 from asgiref.sync import async_to_sync
+from config import DEBUG
 
 
 class jobs:
     scheduler = BackgroundScheduler(timezone='Asia/Tashkent')
     scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
     register_events(scheduler)
-    scheduler.add_job(async_to_sync(
-        atmos_job.update_access_token), 'interval', minutes=0.16)
+    if not DEBUG:
+        scheduler.add_job(async_to_sync(
+            atmos_job.update_access_token), 'interval', minutes=0.16)
