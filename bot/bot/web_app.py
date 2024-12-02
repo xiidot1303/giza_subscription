90dc3c850dd5ab73ed54444f739df28ef44e21d5
xiidot1303 <xii1303@inbox.ru>
2024-12-02 12:33:23 +0500

    remove card from atmos if payment is unsuccessfull

--- bot/bot/web_app.py
+++ bot/bot/web_app.py
@@ -5,6 +5,7 @@ from app.services.plan_service import *
 from app.services.subscription_service import *
 from app.services.channel_access_service import *
 from payment.services.atmos.transaction_api import *
+from payment.services.atmos.card_api import remove_card_api
 from payment.services.card_service import *
 from bot.services.referral_service import referrals_count_of_bot_user
 from config import DEBUG
@@ -15,19 +16,19 @@ async def web_app_data(update: Update, context: CustomContext) -> None:
     data = json.loads(update.effective_message.web_app_data.data)
     card_data: CardData = DictToClass(data["card_data"])
 
-    # create or update card of the user
-    await update_card_of_bot_user(bot_user, card_data)
-    
-    plan_id = context.user_data["plan_id"]
-    channel_id = TG_CHANNEL_ID
-    # get subscription plan object
-    plan: SubscriptionPlan = await get_subscription_plan_by_id(plan_id)
-    # get bot user by update
-    bot_user: Bot_user = await get_user_by_update(update)
-
-    # create payment
-    payment: Payment = await create_payment(bot_user, plan.price)
     try:
+        # create or update card of the user
+        await update_card_of_bot_user(bot_user, card_data)
+
+        plan_id = context.user_data["plan_id"]
+        channel_id = TG_CHANNEL_ID
+        # get subscription plan object
+        plan: SubscriptionPlan = await get_subscription_plan_by_id(plan_id)
+        # get bot user by update
+        bot_user: Bot_user = await get_user_by_update(update)
+
+        # create payment
+        payment: Payment = await create_payment(bot_user, plan.price)
         # create transaction
         transaction_id = await create_transaction_api(payment.id, payment.amount)
         pre_apply = await pre_apply_transaction_api(transaction_id, card_data.card_token)
@@ -36,11 +37,11 @@ async def web_app_data(update: Update, context: CustomContext) -> None:
         transaction_data = await apply_transaction_api(transaction_id)
         assert transaction_data["result"]["code"] == "OK"
 
-        error = None
-
         # set payment as payed
         payment.payed = True
         await payment.asave()
+
+        error = None
     except Exception as ex:
         error = ex
 
@@ -117,6 +118,8 @@ async def web_app_data(update: Update, context: CustomContext) -> None:
         await update_message_reply_text(update, text, reply_markup=markup)
 
     elif error:
+        # remove card
+        await remove_card_api(card_data.card_id, card_data.card_token)
         # error in payment
         text = await GetText.on(Text.error_in_payment)
         await update_message_reply_text(update, text)
