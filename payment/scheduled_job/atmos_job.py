5e35693316e5296893869b7787d05b15474d92d7
xiidot1303 <xii1303@inbox.ru>
2025-01-16 14:04:59 +0500

    Revert "fix updating atmos token"
    This reverts commit d603cdfab227af3385301d335761e725adcd9c99.

--- payment/scheduled_job/atmos_job.py
+++ payment/scheduled_job/atmos_job.py
@@ -1,21 +1,19 @@
 from payment.services.atmos import *
 from django.core.cache import cache
-from config import DEBUG
 
 async def update_access_token():
-    if not DEBUG:
-        # pass if access token is available
-        if not await cache.aget("atmos:access_token"):
-            if refresh_token := await cache.aget("atmos:access_token:copy"):
-                data = {
-                    "grant_type": "client_credentials",
-                    "refresh_token": refresh_token
-                }
-                try:
-                    request = await EndpointRequest.create('token', data, RequestType.POST)
-                    response = await request.send()
-                    access_token = response['access_token']
-                    await cache.aset("atmos:access_token", access_token, timeout=3600)
-                    await cache.aset("atmos:access_token:copy", access_token, timeout=5000)
-                except Exception as ex:
-                    print(ex)
+    # pass if access token is available
+    if not await cache.aget("atmos:access_token"):
+        if refresh_token := await cache.aget("atmos:access_token:copy"):
+            data = {
+                "grant_type": "client_credentials",
+                "refresh_token": refresh_token
+            }
+            try:
+                request = await EndpointRequest.create('token', data, RequestType.POST)
+                response = await request.send()
+                access_token = response['access_token']
+                await cache.aset("atmos:access_token", access_token, timeout=3600)
+                await cache.aset("atmos:access_token:copy", access_token, timeout=5000)
+            except Exception as ex:
+                print(ex)
