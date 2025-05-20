9588ec90faf64f94d4f9eae812a81e4f59e56552
Shakhzod Yuldoshev <65145330+xiidot1303@users.noreply.github.com>
2025-05-20 08:44:21 +0500

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -10,5 +10,5 @@ class app(AppConfig):
     ##     if run_once is not None:
     ##         return
     ##     os.environ['CMDLINERUNNER_RUN_ONCE'] = 'True'
-    #     from app.scheduled_job.updater import jobs
+         from app.scheduled_job.updater import jobs
          jobs.scheduler.start()
